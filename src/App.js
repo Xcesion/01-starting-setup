7d8b2e28a2ad32f1f44d117fb1ec6b66cf217de4
Xcesion <46432761+Xcesion@users.noreply.github.com>
2022-10-10 11:20:31 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import ExpenseGenerator from "./components /ExpenseGenerator";
+
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <p>This is also visable</p>
+      <ExpenseGenerator></ExpenseGenerator>
+    </div>
+  );
+}
+
+export default App;
